1c7c940f0e6afc078ecf785c49375740dac8bf76
Robert Metcalf <rmetcalf9@googlemail.com>
2021-03-21 09:52:40 +0000

    refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
 module "saas_infra" {
-  source = "git::https://github.com/rmetcalf9/tf_saas_service_infra.git?ref=0.0.2"
+  source = "git::https://github.com/rmetcalf9/tf_saas_service_infra.git?ref=0.0.7"
 
   ws_name = var.ws_name
   deployment_config = var.deployment_config
@@ -18,5 +18,12 @@ module "saas_infra" {
   private_allow_tenant_role_whitelist = [
     "saas_shorturl:hasaccount"
   ]
+
+  # main url section
+  mainurl_include = true
+  mainurl = "rjm2.cc"
+  mainurl_majorversion = "0"
+  mainurl_destpath = "/public/api/r/"
+
 }
 
